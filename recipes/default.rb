29359bfc6701787268b024210f25ea023fa7f19f
Seva Feldman <felka@users.noreply.github.com>
2016-02-27 13:08:06 +0200

    Adding template cookbook property

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,7 @@ template '/usr/share/kafka-manager/conf/application.conf' do
     :zkhosts => "#{node['kafka-manager']['zkhosts']}"
   })
   notifies :restart, 'service[kafka-manager]'
+  cookbook node['kafka-manager']['template_source']
 end
 
 service 'kafka-manager' do
