43137b13037d390224cf5768294a0975adc01c58
Seva Feldman <felka@users.noreply.github.com>
2016-02-27 13:10:07 +0200

    Adding cookbook property attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['kafka-manager']['version'] = '1.2.7' #1.2.8 and newer don't scale well with large clusters https://github.com/yahoo/kafka-manager/issues/162
 default['kafka-manager']['zkhosts'] = 'kafka-manager-zookeeper:2181' #comma separated, no spaces
+default['kafka-manager']['template_source'] = "kafka-manager" #override it if you want to place manager config and init file in your cookbook
